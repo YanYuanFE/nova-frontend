304defce83b06062875901eb0a4cf9f48d1f9cb0
YanYuan <yan950414@gmail.com>
2024-09-26 23:14:29 +0800

    fix: fix signin

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -11,23 +11,7 @@ export const metadata = {
 export default function SignIn() {
     return (
         <>
-            <div className="md:hidden">
-                <img
-                    src="/examples/authentication-light.png"
-                    width={1280}
-                    height={843}
-                    alt="Authentication"
-                    className="block dark:hidden"
-                />
-                <img
-                    src="/examples/authentication-dark.png"
-                    width={1280}
-                    height={843}
-                    alt="Authentication"
-                    className="hidden dark:block"
-                />
-            </div>
-            <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+            <div className="container relative h-screen flex flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
                 <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
                     <div className="absolute inset-0 bg-zinc-900" />
                     <div className="relative z-20 flex items-center text-lg font-medium">
